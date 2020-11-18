db599adba2f08dcff455e09a35c3e1dec873d7a6
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2020-11-18 17:48:54 -0500

    Read me update

--- routes/api-routes/expenses.js
+++ routes/api-routes/expenses.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../models");
 
-// /api/commnets/create 
+// /api/comments/create 
 // post comment route -> back to index
 router.post("/create", (req, res, next) => {
    db.Log.create(req.body)
@@ -10,7 +10,6 @@ router.post("/create", (req, res, next) => {
          console.log("[node] new expense:", newExpense.expense);
          res.redirect("/");
       // optionally return data created
-      // res.json(newComment)
       })
       .catch(err => {
          res.status(500);
